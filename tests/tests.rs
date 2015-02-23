5b8a69a828459dd013c9813fd724135ec8e96624
Ahmed Charles <acharles@outlook.com>
2015-02-23 10:46:12 -0800

    Test 'extern crate lua_rs;'.

---
+++ tests/tests.rs
@@ -0,0 +1,10 @@
+// Copyright (C) 2015 Ahmed Charles - acharles@outlook.com
+// Distributed under the MIT License.
+//    (See accompanying file LICENSE.txt or copy at
+//          http://opensource.org/licenses/MIT)
+
+extern crate lua_rs;
+
+#[test]
+fn it_works() {
+}
